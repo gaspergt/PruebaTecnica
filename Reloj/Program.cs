0165158e78767b28d6b9e7b34fe503a74cf07682
luisixc <luislima.ixc@gmail.com>
2024-10-10 11:14:04 -0600

    reloj and prints

---
+++ Reloj/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Hora actual: {0}:{1}:{2}", now.Hour.ToString("D2"), now.Minute.ToString("D2"), now.Second.ToString("D2"));
+            Thread.Sleep(1000); // Pausa de 1 segundo para actualizar la hora
+        }
+    }
+}
