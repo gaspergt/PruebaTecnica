7b7786c6b9d3b7dd13bd05f7b8eb1cf05177b940
Luis Lima <luislima_gt@outlook.com>
2024-10-10 11:14:52 -0600

    fix reloj

--- Reloj/Program.cs
+++ Reloj/Program.cs
@@ -10,7 +10,7 @@ class Program
             Console.Clear();
             DateTime now = DateTime.Now;
             Console.WriteLine("Hora actual: {0}:{1}:{2}", now.Hour.ToString("D2"), now.Minute.ToString("D2"), now.Second.ToString("D2"));
-            Thread.Sleep(1000); // Pausa de 1 segundo para actualizar la hora
+            Thread.Sleep(1000);
         }
     }
 }
